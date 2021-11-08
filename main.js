f734b2e768b880b29beaed133e95433bc0cca63d
Vladyslav <frontendvlados@gmail.com>
2021-11-08 20:13:26 +0200

    change html and JS files and check form

---
+++ main.js
@@ -0,0 +1,21 @@
+document.getElementById("03").addEventListener("click", function() {
+    document.getElementById("second").style.display = "block";
+})
+
+
+
+document.getElementById("03").addEventListener("click", function () {
+    document.getElementById("04").innerHTML = 'Login: ' + document.getElementById("01").value
+    document.getElementById("05").innerHTML = 'Password: ' + document.getElementById("02").value});
+
+
+
+document.getElementById("03").addEventListener("click", function () {
+    localStorage.setItem('Login', (document.getElementById("01").value))
+});
+
+
+
+document.getElementById("03").addEventListener("click", function () {
+    localStorage.setItem('Pasword', (document.getElementById("02").value))
+});
