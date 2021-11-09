1522fc183f3ef0486dcd0103dba7b8df1e720fff
Vladyslav <frontendvlados@gmail.com>
2021-11-09 22:00:34 +0200

    changes

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ document.getElementById("03").addEventListener("click", function() {
     document.getElementById("04").innerHTML = 'Login: ' + document.getElementById("01").value;
     document.getElementById("05").innerHTML = 'Password: ' + document.getElementById("02").value;
     localStorage.setItem('Login', (document.getElementById("01").value));
-    localStorage.setItem('Pasword', (document.getElementById("02").value))
+    localStorage.setItem('Pasword', (document.getElementById("02").value));
 });
